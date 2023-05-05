4c88c7fd4066ee2d0ce21f70152ab852aa1d4fd3
Sydney Alberson <sydney.alberson@gmail.com>
2023-05-04 22:52:19 -0400

    Updated routes with new to_dict method

--- app/routes.py
+++ app/routes.py
@@ -35,11 +35,7 @@ def get_all_books():
 
 
     for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
+        books_response.append(book.to_dict())
 
     return jsonify(books_response)
 
@@ -50,11 +46,7 @@ def read_one_book(book_id):
     
     book = validate_book(book_id)
     
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description
-    }
+    return book.to_dict()
 
 
 def validate_book(book_id):
